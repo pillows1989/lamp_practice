511e9ca987152158e484da46593fa103d5d15086
Daiki Takemura <bareisho.13.jaga@gmail.com>
2019-12-08 14:21:38 +0900

    テーブル設計課題３検討中

---
+++ www/view/history_details_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php include VIEW_PATH . 'templates/head.php'; ?>
+  <title>購入履歴</title>
+  <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'admin.css'); ?>">
+</head>
+<body>
+  <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
+  <h1>購入明細画面</h1>
+
+  <div class="container">
+    <?php include VIEW_PATH . 'templates/messages.php'; ?>
+
+    
+    
+  </div>
+</body>
+</html>
